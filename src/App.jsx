6e0e5161252f23673f19f6df5d89c223d812d978
kim <kimberlypangilinan2001@gmail.com>
2023-07-30 17:22:52 +0800

    meta tags updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,136 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { EditIcon, SmallAddIcon, DeleteIcon } from '@chakra-ui/icons'
-import './App.css'
+import { EditIcon, SmallAddIcon, DeleteIcon } from '@chakra-ui/icons';
+import './App.css';
+
 const App = () => {
+  // State to manage the list of todos
   const [todos, setTodos] = useState(() => {
     const storedTodos = JSON.parse(localStorage.getItem("myTodos"));
     return storedTodos || [];
   });
+
+  // State to manage the input for adding new todo
   const [todo, setTodo] = useState({ text: '', id: Date.now(), isCompleted: false });
+
+  // Reference to the input field for adding new todo
   const inputRef = useRef(null);
 
+  // Save todos in localStorage whenever 'todos' state changes
   useEffect(() => {
-    try{
-      localStorage.setItem("myTodos",JSON.stringify(todos))
-    }catch(e){
-      localStorage.removeItem("myTodos")
+    try {
+      localStorage.setItem("myTodos", JSON.stringify(todos));
+    } catch (e) {
+      // In case there's an error, remove the 'myTodos' from localStorage
+      localStorage.removeItem("myTodos");
     }
-   
-
   }, [todos]);
-  useEffect(() => {
-    try{
-      localStorage.setItem("myTodos",JSON.stringify(todos))
-    }catch(e){
-      localStorage.removeItem("myTodos")
-    }
-   
 
+  // On initial load, retrieve todos from localStorage and set the state
+  useEffect(() => {
+    try {
+      const storedTodos = JSON.parse(localStorage.getItem("myTodos"));
+      if (storedTodos) {
+        setTodos(storedTodos);
+      }
+    } catch (e) {
+      
+      localStorage.removeItem("myTodos");
+    }
   }, []);
+
+  // Add a new todo to the list
   const handleAddTodo = () => {
     if (todo.text.trim() === '') {
       return; // Prevent adding empty todos
     }
 
     setTodos([...todos, todo]);
-    setTodo({ text: '', id: Date.now(), status: false });
+    setTodo({ text: '', id: Date.now(), isCompleted: false });
     inputRef.current.focus();
   };
 
+  // Edit a todo in the list
   const handleEdit = (id, newText) => {
-    setTodos((prevTodos) =>prevTodos.map((todo) => todo.id === id ? { ...todo, text: newText } : todo
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, text: newText } : todo
       )
     );
   };
 
+  // Remove a todo from the list
   const handleRemoveTodo = (id) => {
-    setTodos(prevTodos=>prevTodos.filter(todo=>todo.id != id))
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
+  // Handle checkbox toggle for a todo
   const handleCheckbox = (id) => {
-    setTodos(prevTodos=>prevTodos.map(todo=>todo.id=== id ? {...todo, isCompleted: !todo.isCompleted}:todo))
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
   };
 
+  // Handle form submission for adding a new todo
   const handleOnSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     handleAddTodo();
   };
 
+  // Handle clearing all todos
   const handleClearAll = () => {
-    if (confirm('Are you sure you want to reset your list?')) {
-      setTodos([])
+    if (window.confirm('Are you sure you want to reset your list?')) {
+      setTodos([]);
       localStorage.removeItem("myTodos");
     } else {
-      // Do nothing!
       return;
     }
-    
   };
 
-
-
+  // Render the To-Do List App
   return (
     <>
       <h1>To Do List App</h1>
-      <div class="container">
-      <form onSubmit={handleOnSubmit}>
-        <input
-          type="text"
-          ref={inputRef}
-          placeholder="Enter a task"
-          value={todo.text}
-          onChange={(e) => setTodo({ ...todo, text: e.target.value })}
-        />
-        <button type="submit"><SmallAddIcon/></button>
-        <button class="reset" onClick={handleClearAll}>Reset</button>
-      </form>
-
-      {todos.map((todoitem) => (
-        <div class="todo-item" key={todoitem.id}>
+      <div className="container">
+        <form onSubmit={handleOnSubmit}>
           <input
-            type="checkbox"
-            checked={todoitem.isCompleted}
-            onChange={() => handleCheckbox(todoitem.id)}
+            type="text"
+            ref={inputRef}
+            placeholder="Enter a task"
+            value={todo.text}
+            onChange={(e) => setTodo({ ...todo, text: e.target.value })}
           />
-          <span
-            style={todoitem.isCompleted ? { textDecoration: 'line-through' } : null}
-          >
-            {todoitem.text}
-          </span>
-          
-          <button class="item-button"
-            onClick={() => {
-              const editedText = prompt('Edit the task:', todoitem.text);
-              if (editedText !== null) {
-                handleEdit(todoitem.id, editedText);
-              }
-            }}
-          >
-            <EditIcon/>
-          </button>
-          <button class="item-button" onClick={() => handleRemoveTodo(todoitem.id)}><DeleteIcon /></button>
-        </div>
-      ))}
+          <button type="submit"><SmallAddIcon /></button>
+          <button className="reset" onClick={handleClearAll}>Reset</button>
+        </form>
+
+        {todos.map((todoitem) => (
+          <div className="todo-item" key={todoitem.id}>
+            <input
+              type="checkbox"
+              checked={todoitem.isCompleted}
+              onChange={() => handleCheckbox(todoitem.id)}
+            />
+            <span
+              style={todoitem.isCompleted ? { textDecoration: 'line-through' } : null}
+            >
+              {todoitem.text}
+            </span>
+
+            <button className="item-button"
+              onClick={() => {
+                const editedText = prompt('Edit the task:', todoitem.text);
+                if (editedText !== null) {
+                  handleEdit(todoitem.id, editedText);
+                }
+              }}
+            >
+              <EditIcon />
+            </button>
+            <button className="item-button" onClick={() => handleRemoveTodo(todoitem.id)}><DeleteIcon /></button>
+          </div>
+        ))}
       </div>
     </>
   );
